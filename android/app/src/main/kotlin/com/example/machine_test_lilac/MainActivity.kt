c43f031066d5e29d80dfe245fcbc133d35269dc0
Noel <noel@techwarelab.com>
2023-10-01 17:05:59 +0530

    third commit

--- android/app/src/main/kotlin/com/example/machine_test_lilac/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/machine_test_lilac/MainActivity.kt
@@ -7,8 +7,8 @@ import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
 
-//    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-//        window.addFlags(LayoutParams.FLAG_SECURE)
-//        super.configureFlutterEngine(flutterEngine)
-//    }
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        window.addFlags(LayoutParams.FLAG_SECURE)
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
